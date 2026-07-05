9a89d8838ce7a88f8f8c57b43af34bbf35e83add
varnika114 <145091302+varnika114@users.noreply.github.com>
2026-07-05 21:44:48 +0530

    Add files via upload

---
+++ week -2/LibraryManagement_DependencyInjection/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+import com.library.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        BookService service =
+                context.getBean("bookService", BookService.class);
+
+        service.displayService();
+    }
+}
